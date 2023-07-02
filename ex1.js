d425679563bc705b8c01a33fb9e7b6815cbcc1c8
Jagadeeshram23 <99553806+Jagadeeshram23@users.noreply.github.com>
2023-07-02 23:36:12 +0530

    Add files via upload

---
+++ ex1.js
@@ -0,0 +1,12 @@
+const http = require("http")
+
+const server = http.createServer(function(req,res){
+    console.log("visited");
+    res.writeHead(200, {"Content-Type": "text/html"});
+    res.write("hello world");
+    res.end();
+});
+
+server.listen(9000, ()=> {
+    console.log("running");
+})
