0592bd7a0a7475d99dcb7d61033415f45742cb79
Jagadeeshram23 <99553806+Jagadeeshram23@users.noreply.github.com>
2023-08-07 18:01:25 +0530

    Update ex3node.js

--- ex3node.js
+++ ex3node.js
@@ -8,14 +8,13 @@ var b = Number(prompt('Enter b :'));
 var op = prompt('Enter Operation :');
 switch(op){
     case "1":
-        console.log('adding:'+cal.add(a, b));break;
+        cal.add(a, b);break;
     case "2":
-        cconsole.log('substract:'+cal.sub(a, b));break;
+        cal.sub(a, b);break;
     case "3":
-        console.log('multiply:'+cal.mul(a, b));break;
+        cal.mul(a, b);break;
     case "4":
-        console.log('divide:'+cal.div(a, b));break;
+        cal.div(a, b);break;
 }
-
 //command to run: node ex3node.js
 
