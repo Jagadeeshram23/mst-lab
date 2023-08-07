e042706ef3272b40d033ada7119576653ef4c54e
Jagadeeshram23 <99553806+Jagadeeshram23@users.noreply.github.com>
2023-08-07 17:54:42 +0530

    Update ex3node.js

--- ex3node.js
+++ ex3node.js
@@ -1,19 +1,20 @@
 const prompt = require("prompt-sync")({sigint : true});
 var cal = require("./ex3.js");
 
+
 console.log('1. Add\n2. Sub\n3. Mul\n4. Div');
 var a = Number(prompt("Enter a :"));
 var b = Number(prompt('Enter b :'));
 var op = prompt('Enter Operation :');
 switch(op){
     case "1":
-        cal.add(a,b);break;
+        console.log('adding:'+cal.add(a, b));break;
     case "2":
-        cal.sub(a,b);break;
+        cconsole.log('substract:'+cal.sub(a, b));break;
     case "3":
-        cal.mul(a,b);break;
+        console.log('multiply:'+cal.mul(a, b));break;
     case "4":
-        cal.div(a,b);break;
+        console.log('divide:'+cal.div(a, b));break;
 }
 
 //command to run: node ex3node.js
